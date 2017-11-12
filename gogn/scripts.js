12a2353529ebcf1a089f84b225bc63f2a7cc247f
Rafno <rafnar1993@gmail.com>
2017-11-12 19:36:43 +0000

    Fetch WIP
    Mögulega hætti við þá lausn.

--- gogn/scripts.js
+++ gogn/scripts.js
@@ -1,10 +1,29 @@
 // JavaScript source code
 document.write("Hallo!");
 
-fetch('videos.json')
-  .then(response => response.json())
-  .then(data => {
-    this.videos = data;
-    console.log(videos);
-  })
-  .catch(error => console.log("We failed"));
+/*
+* fetch fall sem sækir videos.json og parsear það fyrir okkur.
+* Helgi, stop trying to make 'fetch' happen, it's not going to happen.
+*/
+function thatIsSoFetch() {
+  return fetch('videos.json').then(function (data) {
+    return data.json();
+  }).then(function (json) {
+    return json;
+  });
+}
+/*Þegar thatIsSoFetch er búið að keyra, afþví það er async, þá keyrir restin af kóðanum.
+*Annars náum við aldrei í JSON.
+*/
+
+var Fetched = Promise.resolve(thatIsSoFetch());
+console.log(Fetched + "Hall");
+function runIt() {
+  thatIsSoFetch().then(function (result) {
+    var days = parseInt(result.videos[0].created);
+    s = result.videos[0].title;
+    //console.log(days);
+  });
+}
+runIt();
+/*----------Ekki kóða neitt nema ef það bíði eftir thatIsSoFetch!!!---------------*/
