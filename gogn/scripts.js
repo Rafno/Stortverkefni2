5e7da665734794e01db9d9fc4b534490645c5751
Rafno <rafnar1993@gmail.com>
2017-11-12 20:27:40 +0000

    Test.html búið til og loksins strokað út allar lint villur.
    fuck u Nodejs

--- gogn/scripts.js
+++ gogn/scripts.js
@@ -1,22 +1,10 @@
 // JavaScript source code
-<<<<<<< HEAD
+
 /**
  * scripts.js tekur inn videos.json og gefur okkur aðgang
-*  Sem fremur að 
+*  Sem fremur að við fáum að bíða eftir videos json.
  */
-=======
-document.write("Hallo!");
 
-<<<<<<< HEAD
-fetch('videos.json')
-  .then(response => response.json())
-  .then(data => {
-    this.videos = data;
-    console.log(videos);
-  })
-  .catch(error => console.log("We failed"));
-=======
->>>>>>> origin/master
 /*
 * fetch fall sem sækir videos.json og parsear það fyrir okkur.
 * Helgi, stop trying to make 'fetch' happen, it's not going to happen.
@@ -36,9 +24,9 @@ function runIt() {
   thatIsSoFetch().then(function (result) {
     var days = parseInt(result.videos[0].created);
     s = result.videos[0].title;
-    //console.log(days);
+    console.log(days);
   });
 }
 runIt();
 /*----------Ekki kóða neitt sem bíður ekki eftir thatIsSoFetch!!!---------------*/
->>>>>>> af9e384504bfd98977c64e4f654a018a2ac1ce08
+
