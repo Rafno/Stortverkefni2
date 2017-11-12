7f4a47c1f67a2a4a69fa95fa7ee5683e5547025a
Rafno <rafnar1993@gmail.com>
2017-11-12 17:41:48 +0000

    I don't know what I am doing.

--- gogn/scripts.js
+++ gogn/scripts.js
@@ -1,2 +1,10 @@
 // JavaScript source code
-document.write("Halló!");
+document.write("Hallo!");
+
+fetch('videos.json')
+  .then(response => response.json())
+  .then(data => {
+    this.videos = data;
+    console.log(videos);
+  })
+  .catch(error => console.log("We failed"));
