8f35ea62ebe202dc0af4fd6553001bbeaf698aad
helgi.96 <helgigretargunnars.96@gmail.com>
2017-11-21 10:32:20 +0000

    video.js
    Ef id er of hátt er komið

--- gogn/videos.js
+++ gogn/videos.js
@@ -8,62 +8,113 @@ console.log("Run the world!");
 const id = urlSplit;
 let teljari = 1;
 /************************** Title **************************** */
-if(urlSplit>3){
-  const headline = createElement("div","headline");
-  const texter = appendChild(headline, "h2","headline");
-  addTextToNode(texter,"Myndbandaleigan");
-  main.appendChild(headline);
-
-} else{
-const video = data.videos[id].video;
-const poster = data.videos[id].poster;
-const titill = data.videos[id].title;
-
-const title = createElement("div","Fyrirsogn");
-const txt = appendChild(title,"h3","title");
-addTextToNode(txt,titill);
-main.appendChild(title);
-
-/************************  Video   ****************************/
-const media = createElement("div", "media");
-main.appendChild(media);
-
-const owerlay = createElement("div","owerlay");
-media.appendChild(owerlay);
-
-const videoDiv = createElement("video", "videoDiv");
-videoDiv.setAttribute("src", video);
-
-
-const posterDiv = createElement("img", "videoDiv");
-posterDiv.setAttribute("src", poster);
-media.appendChild(posterDiv);
-
-
-
-/************container fyrir controls takkana*****************/
+let video;
+let poster;
+/* Create Element  that holds all the wideo media buttons together*/
 const boxer = createElement("div","container");
 appendChild(boxer,"div","boxer");
-main.appendChild(boxer);
 
-/********************** Fast Back Button********************************/
-
+/*Fast back button created*/
 const BackDiv = createElement("img", "pic");
 BackDiv.setAttribute("src","/img/back.svg");
 appendChild(BackDiv,"button","BackDiv");
-boxer.appendChild(BackDiv);
 
+/*Play and pause button created*/
+const playPauseDiv = createElement("img", "pic");
+playPauseDiv.setAttribute("src","/img/play.svg");
+appendChild(playPauseDiv,"button","playPauseDiv");
+
+/*  Container that says til baka */
+const texti =" Til baka ";
+const back = createElement("div","backContainer");
+const texter = appendChild(back,"div","back");
+addTextToNode(texter,texti);
+
+ /* mute and unmute buttons */
+ const muterDiv = createElement("img", "pic");
+ muterDiv.setAttribute("src","/img/mute.svg");
+ appendChild(muterDiv,"button","muterDiv");
+
+ /* Full screen button  */
+ const fulDiv = createElement("img", "pic");
+ fulDiv.setAttribute("src","/img/fullscreen.svg");
+ appendChild(fulDiv,"button","fulkDiv");
+
+ /* Forward Button  */
+ const forwardDiv = createElement("img", "pic");
+ forwardDiv.setAttribute("src","/img/next.svg");
+ appendChild(forwardDiv,"button","forwardDiv");
+
+/* If the id is higher than 3 that means that there is now video awailable in the
+  json file so the program just prints out the message as the title and shows the
+  video media player  */
+if(id > 3){
+  let titill = "Myndabandaleigan";
+  const skilabod = "Videóið er ekki til";
+  const title = createElement("div","Fyrirsogn");
+  const txt = appendChild(title,"h3","title");
+  addTextToNode(txt,titill);
+  main.appendChild(title);
+
+  const textamsg = createElement("div","skilabod");
+  const msg = appendChild(textamsg,"h3","textamsg");
+  addTextToNode(msg,skilabod);
+
+  main.appendChild(textamsg);
+  main.appendChild(boxer);
+  boxer.appendChild(BackDiv);
+  boxer.appendChild(playPauseDiv);
+  main.appendChild(back);
+  boxer.appendChild(muterDiv);
+  boxer.appendChild(fulDiv);
+  boxer.appendChild(forwardDiv);
+
+  back.addEventListener('click', function () {
+    console.log("button has been pressed");
+    window.location.href = ("index.html");
+  });
+
+}
+/*  Otherwise the id was not to high and there is a video in the json file with
+    this id */
+else{
+   video = data.videos[id].video;
+   poster = data.videos[id].poster;
+   let titill = data.videos[id].title;
+
+   const title = createElement("div","Fyrirsogn");
+   const txt = appendChild(title,"h3","title");
+   addTextToNode(txt,titill);
+   main.appendChild(title);
+
+
+  /************************  Video   ****************************/
+  const media = createElement("div", "media");
+  main.appendChild(media);
+
+  const owerlay = createElement("div","owerlay");
+  media.appendChild(owerlay);
+
+  const videoDiv = createElement("video", "videoDiv");
+  videoDiv.setAttribute("src", video);
+
+
+  const posterDiv = createElement("img", "videoDiv");
+  posterDiv.setAttribute("src", poster);
+  media.appendChild(posterDiv);
+/************append container fyrir controls takkana*****************/
+main.appendChild(boxer);
+
+/********************** append Fast Back Button********************************/
+boxer.appendChild(BackDiv);
 /**************** Effect for Back Button ***********************/
 BackDiv.addEventListener('click', function () {
   console.log("button has been pressed");
   videoDiv.currentTime = videoDiv.currentTime-3;
 });
-/************************ Play Button ****************************/
-const playPauseDiv = createElement("img", "pic");
-playPauseDiv.setAttribute("src","/img/play.svg");
-appendChild(playPauseDiv,"button","playPauseDiv");
-boxer.appendChild(playPauseDiv);
 
+/************************ Play Button ****************************/
+boxer.appendChild(playPauseDiv);
 function player(){
   const mainreplace = main.childNodes[0];
   if(teljari ===1){
@@ -87,31 +138,23 @@ function player(){
     document.querySelector(".owerlay").style.zIndex="+1";
     playPauseDiv.setAttribute("src","/img/play.svg");
     console.log("ferðu hinfgar");
-  //  owerlay.style.backgroundImage = "url('/img/play.svg')";
   }
 
 }
-/*****************Effect for play Button***********************/
+/*  calling to addEventListener for 2 kind of play button.
+    one is in the boxer and the other on the video  */
 
 playPauseDiv.addEventListener('click', player);
 owerlay.addEventListener('click',player);
 
-/************ Kassin sem sér um að fara aftur um síðu*******/
-const texti =" Til baka ";
-const back = createElement("div","backContainer");
-const texter = appendChild(back,"div","back");
-addTextToNode(texter,texti);
+/************append Kassin sem sér um að fara aftur um síðu*******/
 main.appendChild(back);
 /******** Efecct fyir kassan sem fer aftur um eina síðu ******/
 back.addEventListener('click', function () {
   console.log("button has been pressed");
   window.location.href = ("index.html");
 });
-/************************ Unmute Button*********************************/
-
-const muterDiv = createElement("img", "pic");
-muterDiv.setAttribute("src","/img/mute.svg");
-appendChild(muterDiv,"button","muterDiv");
+/************************append Unmute Button*********************************/
 boxer.appendChild(muterDiv);
 
 /**************** Effect for unMute Button ***********************/
@@ -122,37 +165,26 @@ muterDiv.addEventListener('click', function () {
     console.log("ýtt á mute");
     videoDiv.muted = true;
     muterDiv.setAttribute("src","/img/unmute.svg");
-    //muterDiv.replaceChild(replace, muterDiv.childNodes[2]);
       }
   else{
     videoDiv.muted = false;
     muterDiv.setAttribute("src","/img/mute.svg");
-  //  muterDiv.replaceChild(replace, muterDiv.childNodes[2]);
   }
 
 
 });
 
 
-/*********************** Full Button****************************/
-
-const fulDiv = createElement("img", "pic");
-fulDiv.setAttribute("src","/img/fullscreen.svg");
-appendChild(fulDiv,"button","fulkDiv");
-boxer .appendChild(fulDiv);
-/************************ Effect Full screen ***********************/
+/*  ********************** append Full Button*************************** */
+boxer.appendChild(fulDiv);
+/************************ Effect Full screen ********************** */
 fulDiv.addEventListener('click', function () {
   const requestFullScreen = videoDiv.requestFullscreen ||
   videoDiv.msRequestFullscreen || videoDiv.mozRequestFullScreen
   || videoDiv.webkitRequestFullscreen;
   requestFullScreen.call(videoDiv);
 });
-/********************** Fast Forward Button ********************************/
-
-
-const forwardDiv = createElement("img", "pic");
-forwardDiv.setAttribute("src","/img/next.svg");
-appendChild(forwardDiv,"button","forwardDiv");
+/*  ********************* append Fast Forward Button ******************************* */
 boxer.appendChild(forwardDiv);
 
 /**************** Effect for Forward Button ***********************/
@@ -163,7 +195,7 @@ forwardDiv.addEventListener('click', function () {
 }
 }
 
-/*Appen, addTetxt og create hjálparföll*/
+/*Append, addTetxt og create hjálparföll*/
 const createElement = function (element, className = "") {
   const div = document.createElement(element);
   div.setAttribute("class", className);
@@ -179,13 +211,6 @@ const appendChild = function (node, elementType, className = "") {
 const addTextToNode = function (node, text) {
   node.appendChild(document.createTextNode(text));
 }
-
-
-
-
-
-
-
 async function init(){
   let response = await fetch('videos.json');
   await response.json()
